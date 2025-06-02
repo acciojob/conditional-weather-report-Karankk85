04c73c0062772ee2076b53b99fa8f11c8c9db933
Karankk85 <karkk8533@gmail.com>
2025-06-02 13:24:01 +0530

    Update App.js
    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import WeatherDisplay from "./components/WeatherDisplay";
+import "./styles/App.css"; 
 
 const App = () => {
+  // Hardcoded weather input
+  const [weather] = useState({
+    temperature: 25,
+    conditions: "Sunny"
+  });
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <WeatherDisplay weather={weather} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
